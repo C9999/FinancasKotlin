43853e262a21944b60f5771779074feb268eb22f
Carlos Araújo <car_soad@hotmail.com>
2018-09-08 21:14:43 -0300

    synthetic e criação de lista simples

--- app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
@@ -2,12 +2,21 @@ package br.com.alura.financask.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
 import br.com.alura.financask.R
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
+
+        val transacoes = listOf("Comida - R$ 20,50", "Economia - R$ 100,00")
+
+        val arrayAdapter = ArrayAdapter(this,
+            android.R.layout.simple_expandable_list_item_1, transacoes)
+
+        lista_transacoes_listview.setAdapter(arrayAdapter)
     }
 }
