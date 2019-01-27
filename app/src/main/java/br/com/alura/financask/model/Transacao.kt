b814c27fb5cc95a1ffae29239e18af9c8ddf653e
Carlos Araújo <car_soad@hotmail.com>
2019-01-26 23:40:48 -0200

    inserindo os demais valores nas transações(simpleDateFormat)

--- app/src/main/java/br/com/alura/financask/model/Transacao.kt
+++ app/src/main/java/br/com/alura/financask/model/Transacao.kt
@@ -3,15 +3,7 @@ package br.com.alura.financask.model
 import java.math.BigDecimal
 import java.util.Calendar
 
-class Transacao (valor: BigDecimal,
-                 categoria: String,
-                 data: Calendar){
-
-    private val valor: BigDecimal = valor
-    private val categoria: String = categoria
-    private val data: Calendar = data
-
-    fun getValor () : BigDecimal{
-        return valor
-    }
+class Transacao (val valor: BigDecimal,
+                 val categoria: String,
+                 val data: Calendar){
 }
--- app/src/main/java/br/com/alura/financask/ui/adapter/ListaTransacoesAdapter.kt
+++ app/src/main/java/br/com/alura/financask/ui/adapter/ListaTransacoesAdapter.kt
@@ -8,6 +8,7 @@ import android.widget.BaseAdapter
 import br.com.alura.financask.R
 import br.com.alura.financask.model.Transacao
 import kotlinx.android.synthetic.main.transacao_item.view.*
+import java.text.SimpleDateFormat
 
 class ListaTransacoesAdapter(transacoes: List<Transacao>,
                              context: Context) : BaseAdapter(){
@@ -20,7 +21,13 @@ class ListaTransacoesAdapter(transacoes: List<Transacao>,
 
         val transacao = transacoes[posicao]
 
-        viewCriada.transacao_valor.setText(transacao.getValor().toString())
+        viewCriada.transacao_valor.text = transacao.valor.toString()
+        viewCriada.transacao_categoria.text = transacao.categoria
+
+        val formatoBrasileiro = "dd/MM/yyyy"
+        val format = SimpleDateFormat(formatoBrasileiro)
+        val dataFormatada = format.format(transacao.data.time)
+        viewCriada.transacao_data.text = dataFormatada
 
 
         return viewCriada
