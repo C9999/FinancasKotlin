2344560648d6781a20fc6fca798b867ba5ced920
Carlos Araújo <car_soad@hotmail.com>
2019-02-07 15:33:13 -0200

    inserindo a cor do total

--- app/src/main/java/br/com/alura/financask/ui/ResumoView.kt
+++ app/src/main/java/br/com/alura/financask/ui/ResumoView.kt
@@ -8,6 +8,7 @@ import br.com.alura.financask.extension.formataParaBrasileiro
 import br.com.alura.financask.model.Resumo
 import br.com.alura.financask.model.Transacao
 import kotlinx.android.synthetic.main.resumo_card.view.*
+import java.math.BigDecimal
 
 class ResumoView(private val view: View,
                  private val context: Context,
@@ -29,6 +30,14 @@ class ResumoView(private val view: View,
 
     fun adicionaTotal() {
         val total = resumo.total()
+
+        if(total.compareTo(BigDecimal.ZERO) >= 0){
+            view.resumo_card_total.setTextColor(ContextCompat.getColor(context, R.color.receita))
+        }else{
+            view.resumo_card_total.setTextColor(ContextCompat.getColor(context, R.color.despesa))
+        }
         view.resumo_card_total.text = total.formataParaBrasileiro()
+
+
     }
 }
