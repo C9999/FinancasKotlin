192d42fb3ecde0e3fb773322ba47e18a5bb0aa56
Carlos Araújo <car_soad@hotmail.com>
2019-02-07 15:14:22 -0200

    adicionando cores na receita e despesa

--- app/src/main/java/br/com/alura/financask/ui/ResumoView.kt
+++ app/src/main/java/br/com/alura/financask/ui/ResumoView.kt
@@ -1,23 +1,29 @@
 package br.com.alura.financask.ui
 
+import android.content.Context
+import android.support.v4.content.ContextCompat
 import android.view.View
+import br.com.alura.financask.R
 import br.com.alura.financask.extension.formataParaBrasileiro
 import br.com.alura.financask.model.Resumo
 import br.com.alura.financask.model.Transacao
 import kotlinx.android.synthetic.main.resumo_card.view.*
 
 class ResumoView(private val view: View,
+                 private val context: Context,
                  transacoes: List<Transacao>) {
 
     private val resumo: Resumo = Resumo(transacoes)
 
     fun adicionaReceita() {
         val totalReceita = resumo.receita()
+        view.resumo_card_receita.setTextColor(ContextCompat.getColor(context, R.color.receita))
         view.resumo_card_receita.text = totalReceita.formataParaBrasileiro()
     }
 
     fun adicionaDespesa() {
         val totalDespesa = resumo.despesa()
+        view.resumo_card_despesa.setTextColor(ContextCompat.getColor(context, R.color.despesa))
         view.resumo_card_despesa.text = totalDespesa.formataParaBrasileiro()
     }
 
