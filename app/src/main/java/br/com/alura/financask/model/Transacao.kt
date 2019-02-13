67fdfc8c58759e16713400c70cc2b34c6040d5b3
Carlos Araújo <car_soad@hotmail.com>
2019-02-13 18:21:33 -0200

    atualizando dependecias para a nova versão do Android Studio 3.2

--- app/src/main/java/br/com/alura/financask/model/Transacao.kt
+++ app/src/main/java/br/com/alura/financask/model/Transacao.kt
@@ -6,9 +6,8 @@ import java.util.Calendar
 class Transacao (val valor: BigDecimal,
                  val categoria: String,
                  val tipo: Tipo,
-                 val data: Calendar = Calendar.getInstance()){
-
+                 var data: Calendar = Calendar.getInstance())
 //    constructor(valor: BigDecimal, tipo: Tipo) : this(valor, "Indefinida", tipo)
 //
 //    constructor(valor: BigDecimal, tipo: Tipo, data: Calendar) : this(valor, "Indefinida", tipo, data)
-}
+
