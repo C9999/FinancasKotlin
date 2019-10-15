c690e492a7d83db6e6d77030b17b7403353f3a2b
C9999 <car_soad@hotmail.com>
2019-10-15 06:20:09 -0300

    cap 7

--- app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
@@ -39,7 +39,17 @@ class ListaTransacoesActivity : AppCompatActivity() {
 
         lista_transacoes_adiciona_receita.setOnClickListener {
             AdicionaTransacaoDialog(window.decorView as ViewGroup, this)
-                    .configuraDialog(object : TransacaoDelegate {
+                    .configuraDialog(Tipo.RECEITA, object : TransacaoDelegate {
+                        override fun delegate(transacao: Transacao) {
+                            atualizaTrasacoes(transacao)
+                            lista_transacoes_adiciona_menu.close(true)
+                        }
+
+                    })
+        }
+        lista_transacoes_adiciona_despesa.setOnClickListener {
+            AdicionaTransacaoDialog(window.decorView as ViewGroup, this)
+                    .configuraDialog(Tipo.DESPESA, object : TransacaoDelegate {
                         override fun delegate(transacao: Transacao) {
                             atualizaTrasacoes(transacao)
                             lista_transacoes_adiciona_menu.close(true)
