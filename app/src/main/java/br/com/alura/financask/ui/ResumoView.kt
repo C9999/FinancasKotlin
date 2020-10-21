d8b87ea8c6bbb5587f3f227e7f951c37af188122
C9999 <car_soad@hotmail.com>
2020-10-21 12:14:48 -0300

    hacktoberfest 1

--- app/src/main/java/br/com/alura/financask/ui/ResumoView.kt
+++ app/src/main/java/br/com/alura/financask/ui/ResumoView.kt
@@ -26,32 +26,26 @@ class ResumoView(private val view: View?,
 
     private fun adicionaReceita() {
         val totalReceita = resumo.receita()
-        view?.let{
-            with(it.resumo_card_receita){
-                setTextColor(corReceita)
-                text = totalReceita.formataParaBrasileiro()
-            }
+        with(view.resumo_card_receita) {
+            setTextColor(corReceita)
+            text = totalReceita.formataParaBrasileiro()
         }
     }
 
     private fun adicionaDespesa() {
         val totalDespesa = resumo.despesa()
-        view?.let{
-            with(view.resumo_card_despesa){
-                setTextColor(corDespesa)
-                text = totalDespesa.formataParaBrasileiro()
-            }
+        with(view.resumo_card_despesa) {
+            setTextColor(corDespesa)
+            text = totalDespesa.formataParaBrasileiro()
         }
     }
 
     private fun adicionaTotal() {
         val total = resumo.total()
         val cor = corPor(total)
-        view?.let{
-            with(it.resumo_card_total){
-                setTextColor(cor)
-                text = total.formataParaBrasileiro()
-            }
+        with(view.resumo_card_total) {
+            setTextColor(cor)
+            text = total.formataParaBrasileiro()
         }
     }
 
